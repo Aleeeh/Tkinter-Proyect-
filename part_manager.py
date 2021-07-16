1d218de59dbff865eec4f0eaf76fb66b1c8c7d66
Alejandro <Aleeeh@users.noreply.github.com>
2021-07-16 01:39:48 +0100

    sql ya funciona

--- part_manager.py
+++ part_manager.py
@@ -12,17 +12,17 @@ conn = sqlite3.connect('address_book.db')
 c = conn.cursor()
 
 # ******** solo necesitamos crearla una vez ******* 
-'''
+"""
 # Create a table 
-c.execute("CREATE TABLE addresses (first_name text, last_name text, address text)")
-'''
+c.execute("CREATE TABLE addresses (Name text, Apellido text, DNI text, Matricula text)")
+"""
 
 def submit(): 
    #Create a database or connect to one 
    conn = sqlite3.connect('address_book.db')
    c = conn.cursor()
    # Insert Into Table
-   c.execute("INSERT INTO addresses VALUES (:name, :apellido_entry, :dni_entry, :matricula_entry)", 
+   c.execute("INSERT INTO addresses VALUES (:name_entry, :apellido_entry, :dni_entry, :matricula_entry)", 
          {
             'name_entry': name_entry.get(),
             'apellido_entry': apellido_entry.get(),
@@ -38,6 +38,21 @@ def submit():
    apellido_entry.delete(0,END)
    dni_entry.delete(0,END)
    matricula_entry.delete(0,END)
+
+
+   # Create Query Function 
+def query():
+   conn = sqlite3.connect('address_book.db')
+   c = conn.cursor()
+   print_records = ""
+   c.execute("SELECT *, oid FROM addresses")
+   records = c.fetchall()
+   print(records)  #just for testing 
+   # Loop thru results
+   # commit and close the data 
+   conn.commit()
+   conn.close()
+
 
 # Widgets - todo lo que le añade a la ventana
 
@@ -66,17 +81,23 @@ dni_entry.grid(row=0, column=4)
 matricula_text = StringVar()
 matricula_label = Label(app, text='Matrícula', font=('bold', 10), pady = 5, padx=20)
 matricula_label.grid(row=1, column=3, sticky=W)
-matricula_entry = Entry(app, textvariable=dni_text)
+matricula_entry = Entry(app, textvariable=matricula_text)
 matricula_entry.grid(row=1, column=4)
 
 # *Botones*
+
 # Quit 
-exit_button = Button(app, text="SalirA", fg="red", command= app.destroy)
-exit_button.grid(row=15, column=0, columnspan=2, padx=20, ipadx=137)
+exit_button = Button(app, text="Salir", fg="red", command= app.destroy)
+exit_button.grid(row=15, column=0, columnspan=2, padx=10, ipadx=137)
 
 # Añadir a la base datos 
 submit_btn = Button(app, text="Añadir a la base de datos", command= submit)
-submit_btn.grid(row = 6, column=0, columnspan=2, pady=20, padx= 10, ipadx= 137)
+submit_btn.grid(row = 6, column=0, columnspan=2, pady=10, padx= 10, ipadx= 137)
+
+# Enseñar los que están en la base de datos 
+query_btn = Button(app, text="Enseñar la base de datos", command= query)
+query_btn.grid(row = 7, column=0, columnspan=2, pady=10, padx= 10, ipadx= 137)
+
 
 app.title('Cool program')
 app.geometry('700x400')
